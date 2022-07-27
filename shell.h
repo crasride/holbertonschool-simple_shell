0fa47ca033d56f21f87e428e81a218e8980ecd23
crasride <jose.dfernandez@yahoo.fr>
2022-07-27 16:51:45 +0200

    Prototype Function

--- shell.h
+++ shell.h
@@ -1,13 +1,11 @@
 #ifndef _SIMPLE_SHELL_
 #define _SIMPLE_SHELL_
 
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <string.h>
-#include <signal.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/wait.h>
+#include<unistd.h>
+#include<string.h>
+#include<ctype.h>
 
 #endif /*_SIMPLE_SHELL_*/
